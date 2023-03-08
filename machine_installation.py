e1e84e35705f694ea99b9955a3f330b5f18b30a9
ADDICTOR <2894515992@qq.com>
2023-03-08 16:49:38 +0800

    [feature : 新增读取配置文件功能]
    3.0版本，新增：
    
    1. 读取配置文件 requirements.txt。

--- machine_installation.py
+++ machine_installation.py
@@ -2,44 +2,50 @@ import subprocess
 
 
 # apt国内源，阿里云，也可更换为其他国内源，加快下载速度
-apt_sources_list = ['deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse\n',
-                    'deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse\n',
-                    'deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse\n',
-                    'deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse\n',
-                    'deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse\n',
-                    'deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse\n',
-                    'deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse\n',
-                    'deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse\n',
-                    'deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse\n',
-                    'deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse]\n']
-
-
+apt_source_list = []
 # 需下载软件列表
-apt_software_list = ['openssh-server','x11vnc','git','supervisor','make','gcc','g++','vim','libudev-dev',
-                    'autoconf','libzmq3-dev','chrome-gnome-shell','libzmq3-dev','net-tools']
-
-
-pip_source = 'https://pypi.tuna.tsinghua.edu.cn/simple'
-
-
+apt_software_list = []
+# pip国内源
+pip_source_list = []
 # python需要安装的环境，目前主流linux系统都自带python3
-python_environment_list = ['flask','pyautogui==0.9.52','pyqtgraph==0.12.2','PyQt==5.14.2','PyQtWebEngine==5.14.0',
-                            'pymongo==3.8.0','pyserial','paho-mqtt','grpcio_tools','protobuf','grpcio','pyzmq',
-                            'python-xlib','modbus_tk','influxdb_client','pypattyrn','Pint','pyinstaller']
-
-
+python_environment_list = []
 # 用户密码
-password = '123456'
+password = []
+
+
+def config_initialization():
+    """配置文件读取，这种思路可以用在很多东西上面
+    """
+    with open('./requirements.txt', 'r') as f:
+        for line in f.readlines():
+            if line == '[APT_SOURCE]\n':
+                operation = apt_source_list
+                continue
+            elif line == '[APT_SOFTWARE]\n':
+                operation = apt_software_list
+                continue
+            elif line == '[PIP_SOURCE]\n':
+                operation = pip_source_list
+                continue
+            elif line == '[PIP_MODULE]\n':
+                operation = python_environment_list
+                continue
+            elif line == '[PASSWORD]':
+                operation = password
+                continue
+            else:
+                operation.append(line.strip('\n'))
 
 
 def apt_switching_source():
     """apt更换国内源
     """
-    subprocess.call(f'echo {password} | sudo -S cp /etc/apt/sources.list /etc/apt/sources.list.bat', shell=True)
+    subprocess.call(f'echo {password[0]} | sudo -S cp /etc/apt/sources.list /etc/apt/sources.list.bat', shell=True)
     with open('./sources.list', 'w') as f:
-        f.writelines(apt_sources_list)
-    subprocess.call(f'echo {password} | sudo -S apt-get update', shell=True)
-    subprocess.call(f'echo {password} | sudo -S apt-get upgrade', shell=True)
+        for s in apt_source_list:
+            f.write(f'{s}\n')
+    subprocess.call(f'echo {password[0]} | sudo -S apt-get update', shell=True)
+    subprocess.call(f'echo {password[0]} | sudo -S apt-get upgrade', shell=True)
 
 
 def apt_software_installation():
@@ -52,7 +58,7 @@ def apt_software_installation():
 def pip_switching_source():
     """pip更换国内源
     """
-    subprocess.call(f'pip3 config set global.index-url {pip_source}',shell=True)
+    subprocess.call(f'pip3 config set global.index-url {pip_source_list[0]}',shell=True)
 
 
 def python_environment_installation():
