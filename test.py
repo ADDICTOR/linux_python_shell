a235fbaeeefeae2bacdba967bbe637e8157b07e8
ADDICTOR <2894515992@qq.com>
2023-03-08 16:50:26 +0800

    [test : 测试文件读写，测试配置文件读取模块]
    此乃草稿纸

---
+++ test.py
@@ -0,0 +1,37 @@
+# with open('./requirements.txt') as f:
+#     data = f.read()
+#     print(type(data))
+#     print(data)
+
+
+# with open('./test.txt','w') as f:
+#     f.write('1\n')
+#     f.write('2')
+
+apt_source_list = []
+apt_software_list = []
+pip_source_list = []
+python_environment_list = []
+
+
+with open('./requirements.txt', 'r') as f:
+    for line in f.readlines():
+        if line == '[APT_SOURCE]\n':
+            operation = apt_source_list
+            continue
+        elif line == '[APT_SOFTWARE]\n':
+            operation = apt_software_list
+            continue
+        elif line == '[PIP_SOURCE]\n':
+            operation = pip_source_list
+            continue
+        elif line == '[PIP_MODULE]\n':
+            operation = python_environment_list
+            continue
+        else:
+            operation.append(line.strip('\n'))
+
+print(apt_source_list)
+print(apt_software_list)
+print(pip_source_list)
+print(python_environment_list)
