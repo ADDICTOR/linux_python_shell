55870911c2ce657d6c2dcc5f71557534f1da3c1f
ADDICTOR <2894515992@qq.com>
2023-03-10 15:05:34 +0800

    [feature : linux一键拉取git仓库]

--- GitLab群组一键拉取/linux_gitlab_repository_init.py
+++ GitLab群组一键拉取/linux_gitlab_repository_init.py
@@ -1,12 +1,28 @@
 import subprocess
 
+name = input("请输入项目名称：") 
+
 linux_passward = ["123qweasdzxc"]
 
-# path = input("请输入群组安装路径")
+username = ["zhouxingjian"]
 
-# subprocess.call(f'cd {path}',shell=True)
+password = ["123qweasdzxc"]
 
 subprocess.call(f'echo {linux_passward} | sudo -S touch /home/.git-credentials')
 
-subprocess.call(f'echo {linux_passward} | sudo -S vim /home/.git-credentials')
+with open('/home/.git-credentials','w') as f:
+    f.write(f'https://{username[0]}:{password[0]}@192.168.11.84')
 
+subprocess.call(f'git config --global credential.helper store', shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/deviceservice.git',shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/deviceshadow.git',shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/ui.git',shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/datamanager.git',shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/scpiservice.git',shell=True)
+
+subprocess.call(f'git clone http://192.168.11.84/{name}/document.git',shell=True)
