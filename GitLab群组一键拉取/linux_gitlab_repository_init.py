bb64031a676a1fa6b84fd0773fef923d1afffbfe
ADDICTOR <2894515992@qq.com>
2023-03-17 09:36:23 +0800

    [feature : update]

--- GitLab群组一键拉取/linux_gitlab_repository_init.py
+++ GitLab群组一键拉取/linux_gitlab_repository_init.py
@@ -1,28 +1,54 @@
+# import subprocess
+
+# name = input("请输入项目名称：") 
+
+# linux_passward = ["123qweasdzxc"]
+
+# username = ["zhouxingjian"]
+
+# password = ["123qweasdzxc"]
+
+# subprocess.call(f'echo {linux_passward} | sudo -S touch /home/.git-credentials')
+
+# with open('/home/.git-credentials','w') as f:
+#     f.write(f'https://{username[0]}:{password[0]}@192.168.11.84')
+
+# subprocess.call(f'git config --global credential.helper store', shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/deviceservice.git',shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/deviceshadow.git',shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/ui.git',shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/datamanager.git',shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/scpiservice.git',shell=True)
+
+# subprocess.call(f'git clone http://192.168.11.84/{name}/document.git',shell=True)
+
+import os
 import subprocess
 
-name = input("请输入项目名称：") 
+name = input('请输入项目名称：')
 
-linux_passward = ["123qweasdzxc"]
+username = 'zhouxingjian'
 
-username = ["zhouxingjian"]
+password = '123qweasdzxc'
 
-password = ["123qweasdzxc"]
+repository_list = ['deviceservice','deviceshadow','ui','datamanager','scpiservice','document','modbusserver','susi4']
 
-subprocess.call(f'echo {linux_passward} | sudo -S touch /home/.git-credentials')
+submodule_list = ['deviceservice','deviceshadow']
 
-with open('/home/.git-credentials','w') as f:
-    f.write(f'https://{username[0]}:{password[0]}@192.168.11.84')
+command = f"git clone http://{username}:{password}@192.168.11.84/{name}/"
 
-subprocess.call(f'git config --global credential.helper store', shell=True)
+path =os.getcwd()
 
-subprocess.call(f'git clone http://192.168.11.84/{name}/deviceservice.git',shell=True)
+for r in repository_list:
+    subprocess.call(f"{command}{r}.git",shell=True)
+    # subprocess.call(f"cd {r}",shell=True)
+    # subprocess.call(f"git submodule init | git submodule update",shell=True)
 
-subprocess.call(f'git clone http://192.168.11.84/{name}/deviceshadow.git',shell=True)
-
-subprocess.call(f'git clone http://192.168.11.84/{name}/ui.git',shell=True)
-
-subprocess.call(f'git clone http://192.168.11.84/{name}/datamanager.git',shell=True)
-
-subprocess.call(f'git clone http://192.168.11.84/{name}/scpiservice.git',shell=True)
-
-subprocess.call(f'git clone http://192.168.11.84/{name}/document.git',shell=True)
+for s in submodule_list:
+    subprocess.call("git submodule init",shell=True,cwd=f"{path}/{s}")
+    subprocess.call("git submodule update",shell=True,cwd=f"{path}/{s}")
